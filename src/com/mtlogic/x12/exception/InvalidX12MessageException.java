4f2ef8374f15c994f687cfd7812f1e32c5c4a46d
Mike Trentman <Mike.Trentman@alveohealth.com>
2016-12-15 13:23:07 -0500

    Added serialVersionUID to InvalidX12MessageException

--- src/com/mtlogic/x12/exception/InvalidX12MessageException.java
+++ src/com/mtlogic/x12/exception/InvalidX12MessageException.java
@@ -1,6 +1,8 @@
 package com.mtlogic.x12.exception;
 
 public class InvalidX12MessageException extends Exception {
+	private static final long serialVersionUID = 9216006553718942042L;
+
 	public InvalidX12MessageException(String message) {
 		super(message);
 	}
